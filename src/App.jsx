510dad121cc08fb182ea3f34fe75c263db6e6a80
cameochoquer <cjchoquer@gmail.com>
2023-06-13 12:29:53 +0100

    added player component and user context

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+
+import {createContext, useState} from 'react'
 import Maze from './components/Maze.jsx'
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
 function App() {
+    const [user, setUser] = useState({name: '', emoji: ''})
+    const userContext = createContext()
+
     return (
-        <>
+        <userContext.Provider value={[user, setUser]}>
             <Header />
             <Maze />
             <Footer />
-        </>
+        </userContext.Provider>
     )
 }
 
---
+++ src/components/Player.jsx
@@ -0,0 +1,9 @@
+
+const Player = ({emoji})=>{
+    return (
+        <div>
+            <p>{emoji}</p>
+        </div>
+    )
+}
+export default Player
