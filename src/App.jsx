3f82b4679a06ac1ddd9609a93f74a18aab2d787e
cameochoquer <cjchoquer@gmail.com>
2023-06-13 16:16:33 +0100

    "feature: IT MOVES!!!"

--- src/App.jsx
+++ src/App.jsx
@@ -14,24 +14,26 @@ function App() {
     const [position, setPosition] = useState({ row: 0, column: 0 })
     function movePlayer() {
         const handleKeyDown = (e) => {
-            console.log(e)
             // Define a mapping between keys and actions
             const keyMap = {
-                ArrowUp: { row: position.row--, column: position.column },
-                ArrowDown: { row: position.row++, column: position.column },
-                ArrowLeft: { row: position.row, column: position.column-- },
-                ArrowRight: { row: position.row, column: position.column++ },
+                ArrowUp: { row: position.row -1, column: position.column },
+                ArrowDown: { row: position.row +1, column: position.column },
+                ArrowLeft: { row: position.row, column: position.column -1 },
+                ArrowRight: { row: position.row, column: position.column +1},
             }
 
             // Check if the pressed key is in the keyMap
             if (keyMap.hasOwnProperty(e.key)) {
                 const newPos = keyMap[e.key]
+
                 // Verify the new position is within the boundaries and is not a wall
                 if (
-                    level1[newPos.row] &&
+                    level1[newPos.row] !== undefined && level1[newPos.row][newPos.column] !== undefined &&
                     level1[newPos.row][newPos.column] !== 1
                 ) {
                     setPosition(newPos)
+                } else{
+                   return console.log('bad move')
                 }
             }
             return true
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,7 +3,6 @@ import { UserContext } from '../Context.js'
 
 const Player = () => {
     const [user] = useContext(UserContext)
-    console.log(user.emoji)
     return (
         <div key="me" className="cell player path">
             <p>{user.emoji}</p>
