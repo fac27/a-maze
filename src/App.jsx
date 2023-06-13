3e8f2d2c424274a49eff9de37f790a99fb3a1d5b
cameochoquer <cjchoquer@gmail.com>
2023-06-13 11:00:16 +0100

    feature: added skeleton for header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
     const [count, setCount] = useState(0)
 
     return (
         <>
-
+<Header/>
             <h1>Vite + React</h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () =>{ 
+return (
+    <header>
+        <p>name</p>
+        <h1>A-MAZE</h1>
+        <p>score</p>
+    </header>
+)
+}
+
+export default Header
