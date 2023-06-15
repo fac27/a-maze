c82e2735d0a1bc72c4b1f0895d39f0e81fae9148
FomasTreeman <tom@team-freeman.com>
2023-06-15 11:58:37 +0100

    best score

--- src/components/Win.jsx
+++ src/components/Win.jsx
@@ -10,21 +10,29 @@ export default function Win({
     setTimeElapsed,
     movesMade,
 }) {
+    const score = Math.floor(MAX_SCORE / timeElapsed / movesMade.current)
+
     function restart() {
         setPosition({ row: 0, column: 0 })
         setHasWon(false)
         setTimeElapsed(0)
         movesMade.current = 0
     }
+
+    function saveScore() {
+        const bestScore = localStorage.getItem('best_score')
+        const isBest = bestScore || bestScore > score
+        if (!isBest) localStorage.setItem('best_score', score)
+    }
+
+    saveScore()
+
     return (
         <>
             <Confetti width={window.innerWidth} height={window.innerHeight} />
             <article className="modal">
                 <h2> YoU bEAt mE !</h2>
-                <h3 className="shout">
-                    SCORE:{' '}
-                    {Math.floor(MAX_SCORE / timeElapsed / movesMade.current)}
-                </h3>
+                <h3 className="shout">SCORE: {score}</h3>
                 <button className="restart-btn" onClick={restart}>
                     wont beat me again!
                 </button>
